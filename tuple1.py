019acc1c89e97e571436c9e522dabf18175f1177
Samnji <84843673+Samnji@users.noreply.github.com>
2022-05-07 11:41:48 +0300

    Tuple 1

---
+++ tuple1.py
@@ -0,0 +1,12 @@
+# Print the index of country from a tuple of countries
+
+countries = ("Kenya", "Uganda ", "Tanzania", "Somalia")
+
+print(countries)
+country = input("Choose a country name: ")
+
+print(countries.index(country))
+
+num = int(input("Enter position of a country: "))
+
+print(countries[num])
